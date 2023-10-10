6bee8df9110b84afeb2d450023f28a67d2587f46
Saswata Alex Majumder <majumder.ua@gmail.com>
2023-10-09 23:02:27 -0400

    array filter lab done

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution here
+
+function findMatching(roster, naam){
+ //   console.log(roster);
+    const matches = roster.filter( function (driver){
+        //console.log("in FM's CB: ", driver, naam);
+        return (driver.toUpperCase() === naam.toUpperCase());
+    });
+ //   console.log(matches);
+    return matches;
+}
+function fuzzyMatch(roster, naam){
+ //   console.log(roster);
+    const matches = roster.filter(function (driver){
+        let lookFor = naam.toUpperCase();
+        let lookIn = driver.toUpperCase();
+        return (lookIn.search(lookFor) === 0);
+    });
+ //   console.log(naam, "could be: ", matches);
+    return matches;
+}
+
+function matchName(roster, naam){
+   //  console.log(roster);
+     const matches = roster.filter( function (driver){
+    //  console.log("in FM's CB: ", driver, naam);
+        return (driver.name.toUpperCase() === naam.toUpperCase());
+    });
+ //   console.log(matches);
+    return matches;
+}
